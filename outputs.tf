001649df336eac744e737c7ce4d0aa555eba9ea0
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 19:31:12 +0200

    autoscaling

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "instance_ami" {
-  value = aws_instance.blog.ami
-}
+#output "instance_ami" {
+#  value = aws_instance.blog.ami
+#}
 
-output "instance_arn" {
-  value = aws_instance.blog.arn
-}
+#output "instance_arn" {
+#  value = aws_instance.blog.arn
+#}
 
 #output "selected_region" {
 #  value = aws_region.current.name
