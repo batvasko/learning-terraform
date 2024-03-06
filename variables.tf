cb9882290e536874eeee5aa50c2a93ea105153d6
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 13:39:03 +0200

    change region

--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
+#variable "instance_type" {
+#  description = "Type of EC2 instance to provision"
+#  default     = "t3.nano"
+#  availability_zone  = "eu-west-1a"  # Explicitly set the desired zone
+#}
+
 variable "instance_type" {
-  description = "Type of EC2 instance to provision"
+  description = "Value of the Name tag for the EC2 instance type"
+  type        = string
   default     = "t3.nano"
-  availability_zone  = "eu-west-1a"  # Explicitly set the desired zone
+}
+
+variable "az_1a" {
+  description = "availability zone 1"
+  type        = string
+  default     = "eu-west-1"
 }
