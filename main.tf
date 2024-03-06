d7e35a940c5c43a7e1139a7cdb0b1e2474c99bc7
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 13:11:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  availability_zone = "eu-west-1a"  # Explicitly set the desired zone
 
   tags = {
     Name = "HelloWorld"
