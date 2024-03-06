6e96b88ee701d22cc8024b4b90f7da411ff318a9
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 13:22:23 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,11 @@
-#output "instance_ami" {
-#  value = aws_instance.web.ami
-#}
+output "instance_ami" {
+  value = aws_instance.web.ami
+}
 
-#output "instance_arn" {
-#  value = aws_instance.web.arn
-#}
+output "instance_arn" {
+  value = aws_instance.web.arn
+}
+
 output "selected_region" {
   value = aws_region.current.name
 }
