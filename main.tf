32a6c69fa5795a0618097ea8b557219fa3df49bd
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 18:12:55 +0200

    creat new dev vpc using module

--- main.tf
+++ main.tf
@@ -20,9 +20,30 @@ data "aws_vpc" "default" {
 }
 
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] # using the servers only on public subntes
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true # not being used
+  # enable_vpn_gateway = true # not being used
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  subnet_id = module.blog_vpc.public_subnets[0]
   # vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
@@ -37,7 +58,8 @@ module "blog_sg" {
   version = "5.1.1"
 
   name    = "blog_new"
-  vpc_id  = data.aws_vpc.default.id
+  # vpc_id  = data.aws_vpc.default.id # replaced with the vpc created with module
+  vpc_id  = module.blog_vpc.vpc_id
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
