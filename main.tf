b6237eb2d3a918c4f779cfe17474f147b3d813e8
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 18:47:40 +0200

    alb fix 2

--- main.tf
+++ main.tf
@@ -76,11 +76,9 @@ module "blog_alb" {
       }
     }
   http_tcp_listeners = {
-    {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
-    }
   }
 
   tags = {
