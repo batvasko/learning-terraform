63ef4b15f7b78411742ebfa9848ef0e9a4936a40
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 19:10:03 +0200

    comment http_tcp_listeners in alb

--- main.tf
+++ main.tf
@@ -74,13 +74,13 @@ module "blog_alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  #http_tcp_listeners = [
+  #  {
+  #    port               = 80
+  #    protocol           = "HTTP"
+  #    target_group_index = 0
+  #  }
+  #]
 
   tags = {
     Environment = "dev"
