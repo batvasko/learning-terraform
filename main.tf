375eb00f32144ca878007705ff5ec20725604133
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 16:52:02 +0200

    change to region in provider only

--- main.tf
+++ main.tf
@@ -16,10 +16,10 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  availability_zone = var.az_1a
   instance_type = var.instance_type
   # instance_type = "t3.nano"
   # availability_zone = "eu-west-1a"  # Explicitly set the desired zone
+  # availability_zone = var.az_1a
 
   tags = {
     Name = "Learning Terraform 01"
