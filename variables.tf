74b9bab406b32e81be1f19c9a9861d51c8fb4997
batvasko <33954845+batvasko@users.noreply.github.com>
2024-03-06 13:22:00 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+  availability_zone  = "eu-west-1a"  # Explicitly set the desired zone
+}
